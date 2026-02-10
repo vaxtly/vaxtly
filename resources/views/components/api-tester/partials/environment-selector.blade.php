3a22ae1e754041898bdc539bc13cc2347191e383
Vextly <vextly.app@gmail.com>
2026-02-10 15:41:08 -0300

    change env selector size

--- resources/views/components/api-tester/partials/environment-selector.blade.php
+++ resources/views/components/api-tester/partials/environment-selector.blade.php
@@ -121,12 +121,12 @@
                    border-gray-300 dark:border-gray-600
                    text-gray-700 dark:text-gray-300
                    hover:bg-gray-50 dark:hover:bg-gray-700
-                   focus:outline-none focus:ring-2 focus:ring-blue-500/50"
+                   focus:outline-none focus:ring-2 focus:ring-blue-500/50 min-w-60 justify-between"
         >
             <svg class="w-4 h-4 text-gray-500 dark:text-gray-400 shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
             </svg>
-            <span class="truncate max-w-24" x-text="activeName"></span>
+            <span class="truncate max-w-56" x-text="activeName"></span>
             <svg
                 class="w-4 h-4 text-gray-400 shrink-0 transition-transform"
                 :class="{ 'rotate-180': open }"
